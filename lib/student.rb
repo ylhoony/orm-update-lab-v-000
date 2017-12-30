bc91dada43614a29464f4061da112142f2010fb5
Hoon Lee <yl.hoony@gmail.com>
2017-12-30 01:18:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,11 @@
 require_relative "../config/environment.rb"
 
-require "pry"
-
 class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
   attr_accessor :name, :grade, :id
-  # attr_reader :id
 
   def initialize(name, grade)
     @id = nil
